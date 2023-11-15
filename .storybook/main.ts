ae93303eec8fa756cf4e0a1fa616e0291b073962
Davi Fernandes <davifncosta@gmail.com>
2023-11-15 16:28:00 -0300

    raw: config the storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+import path from "path";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -12,8 +14,13 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+  webpackFinal: async (config, { configType }) => {
+    if (config.resolve)
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src/"),
+      };
+    return config;
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,17 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react'
+
+import '../src/styles/global.scss'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
