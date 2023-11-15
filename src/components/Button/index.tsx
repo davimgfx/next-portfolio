dfc4bd3b99aed1cf6aa83d8838040a8058d90d2e
Davi Fernandes <davifncosta@gmail.com>
2023-11-15 15:25:49 -0300

    feat: add component Hero and Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export const Button = ({ children }: ButtonProps) => {
+  return <button>{children}</button>;
+};
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+export const Hero = () => {
+  return (
+    <section>
+      <h2>REACT DEVELOPER</h2>
+      <div></div>
+      <p>
+        The World Wide Fund for Nature (WWF) is an international organization
+        working on issues regarding the conservation, research and restoration
+        of the environment, formerly named the World Wildlife Fund. WWF was
+        founded in 1961.
+      </p>
+    </section>
+  );
+};
