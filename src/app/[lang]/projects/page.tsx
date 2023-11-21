24492ce51f6d7c41be685c3582278969835168f4
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 19:10:30 -0300

    feat: add params to the head

--- src/app/[lang]/projects/page.tsx
+++ src/app/[lang]/projects/page.tsx
@@ -2,15 +2,16 @@
 import React, { useContext } from "react";
 
 import { Locale } from "../../../config/i18n.config";
-import { Header } from "@/components";
+import { Header, ProjectsAbout } from "@/components";
 import { ThemeContext } from "@/context/ThemeContext";
+
 
 const Projects = ({ params }: { params: { lang: Locale } }) => {
   const { theme } = useContext(ThemeContext);
   return (
     <div id={theme}>
-      <Header />
-      <h2>page</h2>
+      <Header params={{ lang: params.lang }}/>
+      <ProjectsAbout params={{ lang: params.lang }}/>
     </div>
   );
 };
