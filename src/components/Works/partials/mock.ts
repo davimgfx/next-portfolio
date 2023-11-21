b7920704fbf871d73eac3472c8f3583424aa60a6
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 12:49:03 -0300

    fix: works mock

--- src/components/Works/partials/mock.ts
+++ src/components/Works/partials/mock.ts
@@ -4,16 +4,22 @@ export const projectsMock = [
   {
     src: aumigos,
     alt: "aumigos_project",
+    href: "https://aumigos-casa.vercel.app/",
     tags: ["react", "firebase", "tailwind"],
+    hoverEffect: "aumigos"
   },
   {
     src: cryptoNeves,
     alt: "cryptoNeves_project",
+    href: "https://crypto-neves.vercel.app/",
     tags: ["react", "firebase", "material-ui"],
+    hoverEffect: "cryptoNeves"
   },
   {
     src: pulseGym,
     alt: "pulseGym_project",
+    href: "https://davimgfx.github.io/pulseGym/",
     tags: ["react", "tailwind"],
+    hoverEffect: "pulseGym"
   },
 ];
