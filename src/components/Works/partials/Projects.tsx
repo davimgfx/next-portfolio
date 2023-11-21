7d6149ca0ac42cfcc79bf8bed0f616a1e441237f
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 19:12:21 -0300

    feat: add Works component

--- src/components/Works/partials/Projects.tsx
+++ src/components/Works/partials/Projects.tsx
@@ -1,13 +1,13 @@
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { projectsMock } from "./mock";
 
-interface ProjectsProps{
+interface ProjectsProps {
   quantity: number;
 }
 
-export const Projects = ({quantity} : ProjectsProps) => {
+export const Projects = ({ quantity }: ProjectsProps) => {
   const visibleProjects = projectsMock.slice(0, quantity);
 
   return (
@@ -15,14 +15,14 @@ export const Projects = ({quantity} : ProjectsProps) => {
       {visibleProjects.map((project) => (
         <div key={project.alt} className={styles.project}>
           <div className={styles.projectContainerImage}>
-            <Link href={project.href} target="_blanket">
-              <div className={styles.projectImage} >
+            <Link href={project.href} target="_blank">
+              <div className={styles.projectImage}>
                 <Image
                   src={project.src}
                   alt={project.alt}
                   className={`${styles.image} ${styles[project.hoverEffect]}`}
                 />
-              </div >
+              </div>
             </Link>
           </div>
           <div className={styles.projectDescription}>
@@ -31,6 +31,13 @@ export const Projects = ({quantity} : ProjectsProps) => {
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
               voluptate.
             </p>
+          </div>
+          <div className={styles.tags}>
+            {project.tags.map((tag) => (
+              <p key={tag} className={styles.tag}>
+                {tag}
+              </p>
+            ))}
           </div>
         </div>
       ))}
--- src/components/Works/partials/mock.ts
+++ src/components/Works/partials/mock.ts
@@ -5,21 +5,21 @@ export const projectsMock = [
     src: aumigos,
     alt: "aumigos_project",
     href: "https://aumigos-casa.vercel.app/",
-    tags: ["react", "firebase", "tailwind"],
+    tags: ["React", "Firebase", "Tailwind"],
     hoverEffect: "aumigos"
   },
   {
     src: cryptoNeves,
     alt: "cryptoNeves_project",
     href: "https://crypto-neves.vercel.app/",
-    tags: ["react", "firebase", "material-ui"],
+    tags: ["React", "Firebase", "Material-ui"],
     hoverEffect: "cryptoNeves"
   },
   {
     src: pulseGym,
     alt: "pulseGym_project",
     href: "https://davimgfx.github.io/pulseGym/",
-    tags: ["react", "tailwind"],
+    tags: ["React", "Tailwind"],
     hoverEffect: "pulseGym"
   },
 ];
