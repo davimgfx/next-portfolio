60e54de915592af02ad7a38e6686796d3ed73dfd
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 12:45:36 -0300

    feat: add more translations

--- src/dictionaries/default-language-collections/default-en-US.ts
+++ src/dictionaries/default-language-collections/default-en-US.ts
@@ -1,6 +1,59 @@
 export const enUS = {
-  site: {
-    name: "EN String compilation",
-    description: "en-US"
+  navbar: {
+    home: "Home",
+    projects: "Projects",
+    about: "About",
+  },
+  hero: {
+    "desc {{time}}":
+      "Hey. I'm Davi Fernandes. A Front-end developer with {{time}} of experience.",
+    buttons: ["See my Projects", "See CV"],
+  },
+  skills: {
+    desc: "Here, you can find a list of the hard skills I use daily:",
+  },
+  works: {
+    title_part1: "My recent",
+    title_part2: " works",
+    desc: "Here you can see my last works, if you want to see more:",
+    button: "All my projects",
+  },
+  footer: {
+    title_part1: "Got a project in ",
+    title_part2: "mind?",
+    desc: "Don’t be shy! Hit me up!",
+    "copyright {{year}}":
+      "Copyright © {{year}} by Davi Fernandes | All rights are reserved",
+  },
+  about: {
+    title_part1: "About",
+    title_part2: " me",
+    subtitle: "A little bit more about me :)",
+    "paragraph1 {{age}}":
+      "I'm {{age}} years old, and I'm a Software Engineering student currently enrolled in a higher education institution, currently in my first semester of an eight-semester program.",
+    paragraph2:
+      "As a front-end developer, my main goal is to create a pleasant user experience. I'm here to help you turn your ideas into reality and bring life to your projects!",
+    paragraph3:
+      "My current focus is to refine my skills in the technologies I already master and become a Full-Stack Developer.",
+    subtitle2: "Social Medias",
+    subtitle3_part1: "Work",
+    subtitle3_part2: " experience",
+    tech: "Techs:",
+    jobs: {
+      union: {
+        position: "Front end Developer",
+        date: "Set 2023 -> Dez 2023",
+        local: "Remote",
+        goals:
+          "Desenvolvimento junto com a equipe, de uma landing page, aplicando o TDD, para uma empresa de serviços de telefones. Desenvolvi o projeto estabelecido com metodologias Ágeis, Scrum, TDD e mobile first.",
+      },
+      freelance: {
+        position: "Front end Developer",
+        date: "Jan 2023 -> Present",
+        local: "Remote",
+        goals:
+          "Desenvolvimento de aplicações web responsivas garantindo uma experiência otimizada para os usuários em diferentes dispositivos. Foco em qualidade, prazo e respeito pelo cliente.",
+      },
+    },
   },
 };
--- src/dictionaries/default-language-collections/default-pt-BR.ts
+++ src/dictionaries/default-language-collections/default-pt-BR.ts
@@ -1,6 +1,53 @@
 export const ptBR = {
-  site: {
-    name: "BR String compilation",
-    description: "pt-BR"
+  navbar: {
+    home: "Home",
+    projects: "Projetos",
+    about: "Sobre",
   },
+  hero: {
+    "desc {{time}}":
+      "Olá. Eu sou Davi Fernandes. Um desenvolvedor Front-end com {{time}} de experiência.",
+    buttons: ["Veja meus projetos", "Veja meu curriculo"],
+  },
+  skills: {
+    desc: "Aqui, você pode encontrar algumas techs que uso diariamente:",
+  },
+  works: {
+    title_part1: "Trabalhos",
+    title_part2: " recentes",
+    desc: "Aqui, você pode ver, meus trabalhos recentes:",
+    button: "Todos meus projects",
+  },
+  footer: {
+    title_part1: "Tem uma ideia de",
+    title_part2: " projeto?",
+    desc: "Não tenha vergonha, entre em contato!",
+    "copyright {{year}}" : "Copyright © {{year}} por Davi Fernandes | Todos os direitos Reservados"
+  },
+  about: {
+    title_part1: "Sobre",
+    title_part2: " Mim",
+    subtitle: "Um pouco mais sobre mim :)",
+    "paragraph1 {{age}}": "Tenho {{age}} anos, e estou cursando Engenharia de Software, atualmente no segundo semestre de um programa de oito semestres.",
+    paragraph2: "Como desenvolvedor front-end, meu principal objetivo é criar uma experiência agradável para o usuário. Estou aqui para ajudá-lo a transformar suas ideias em realidade e dar vida aos seus projetos!",
+    paragraph3: "Meu foco atual é me aperfeiçoar nas tecnologias que já domino, e virar um Full-Stack Developer.",
+    subtitle2: "Redes socias",
+    subtitle3_part1: "Experiência de",
+    subtitle3_part2: " Trabalho",
+    tech: "Tecnologias:",
+    jobs: {
+      union: {
+        position: "Front end Developer",
+        date: "Set 2023 -> Dez 2023",
+        local: "Remoto",
+        goals: "Desenvolvimento junto com a equipe, de uma landing page, aplicando o TDD, para uma empresa de serviços de telefones. Desenvolvi o projeto estabelecido com metodologias Ágeis, Scrum, TDD e mobile first.",
+      },
+      freelance: {
+        position: "Front end Developer",
+        date: "Jan 2023 -> Presente",
+        local: "Remoto",
+        goals: "Desenvolvimento de aplicações web responsivas garantindo uma experiência otimizada para os usuários em diferentes dispositivos. Foco em qualidade, prazo e respeito pelo cliente.",
+      }
+    }
+  }
 };
