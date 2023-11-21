d8e26a6ea0d8d101a104b05ace8514609dfa9f5d
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 16:00:48 -0300

    feat: add imports for the language

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -8,16 +8,16 @@ import { ThemeContext } from "@/context/ThemeContext";
 
 import { Locale } from "@/config/i18n.config";
 
-export default function Home({ params } : {params: {lang: Locale}}) {
+export default function Home({ params }: { params: { lang: Locale } }) {
   const { theme } = useContext(ThemeContext);
 
   return (
     <div id={theme}>
-      <Header params={{ lang: params.lang }}/>
-      <Hero params={{ lang: params.lang }}/>
-      <Skills params={{ lang: params.lang }}/>
-      <Works params={{ lang: params.lang }}/>
-      <Footer params={{ lang: params.lang }}/>
+      <Header params={{ lang: params.lang }} />
+      <Hero params={{ lang: params.lang }} />
+      <Skills params={{ lang: params.lang }} />
+      <Works params={{ lang: params.lang }} />
+      <Footer params={{ lang: params.lang }} />
     </div>
   );
 }
