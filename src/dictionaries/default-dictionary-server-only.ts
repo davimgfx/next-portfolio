c6ff775ce239a978534909a6ed035feef93885cd
Davi Fernandes <davifncosta@gmail.com>
2023-11-21 10:01:48 -0300

    feat: add interpolation function

--- src/dictionaries/default-dictionary-server-only.ts
+++ src/dictionaries/default-dictionary-server-only.ts
@@ -1,10 +1,10 @@
-import "server-only";
-import { Locale, i18n } from "@/config/i18n.config";
+import 'server-only';
+import { Locale, i18n } from '@/config/i18n.config';
 
-import { defaultDictionary } from "./default-dictionaries";
+import { defaultDictionary } from './default-dictionaries';
+import { interpolation } from './interpolation';
 
-export const getDictionaryServerOnly = async (locale: Locale) => {
-  return (
-    defaultDictionary[locale] ?? defaultDictionary[i18n.defaultLocale as Locale]
-  );
+export const getDictionaryServerOnly = (locale: Locale) => {
+  const dictionary = defaultDictionary[locale] ?? defaultDictionary[i18n.defaultLocale as Locale];
+  return { dictionary, interpolation };
 };
