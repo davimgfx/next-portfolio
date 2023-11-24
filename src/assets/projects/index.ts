b524ecebf01c0ef4d2b3b732b16f532c9867ae5b
Davi Fernandes <davifncosta@gmail.com>
2023-11-24 15:57:29 -0300

    feat: add new images projects

--- src/assets/projects/index.ts
+++ src/assets/projects/index.ts
@@ -1,5 +1,8 @@
 import aumigos from "./aumigos_project.png"
 import cryptoNeves from "./crypto_neves_project.png"
 import pulseGym from "./pulse_gym_project.png"
+import amazonClone from "./amazon_project.png"
+import hoobank from "./hoobank_project.png"
+import toDo from "./to-do_project.png"
 
-export { aumigos, cryptoNeves, pulseGym }
+export { aumigos, cryptoNeves, pulseGym, amazonClone, hoobank, toDo }
